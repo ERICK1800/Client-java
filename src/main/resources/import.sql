ca7380efcc52a58e0ad014f9658b82d2bee04093
ERICK1800 <ericklimaflorencio@gmail.com>
2023-05-30 10:57:38 -0300

    service

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO TBL_CLIENT (ID, NAME, EMAIL) VALUES (1,'Maria','maria@gmail.com');
+INSERT INTO TBL_CLIENT (ID, NAME, EMAIL) VALUES (2,'Pedro','pedro@gmail.com');
+INSERT INTO TBL_CLIENT (ID, NAME, EMAIL) VALUES (3,'Augusto','augusto@gmail.com');
