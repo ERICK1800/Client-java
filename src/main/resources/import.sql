8da9954b687a1359539ef7b87a01e9c14d945207
ERICK1800 <ericklimaflorencio@gmail.com>
2023-06-18 12:14:07 -0300

    fim

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO TBL_CLIENT (ID, NAME, EMAIL) VALUES (1,'Maria','maria@gmail.com');
-INSERT INTO TBL_CLIENT (ID, NAME, EMAIL) VALUES (2,'Pedro','pedro@gmail.com');
-INSERT INTO TBL_CLIENT (ID, NAME, EMAIL) VALUES (3,'Augusto','augusto@gmail.com');
+INSERT INTO TBL_CLIENT (NAME, EMAIL) VALUES ('Maria','maria@gmail.com');
+INSERT INTO TBL_CLIENT (NAME, EMAIL) VALUES ('Pedro','pedro@gmail.com');
+INSERT INTO TBL_CLIENT (NAME, EMAIL) VALUES ('Augusto','augusto@gmail.com');
